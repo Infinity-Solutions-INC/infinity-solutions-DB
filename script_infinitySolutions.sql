8e5a0ddbdd1dca5b0b1c534f20cc57f30adfff39
m4yk0n <maykon.rodrigues@sptech.school>
2024-11-28 18:44:20 -0300

    fix: modificando id usuarios para alinhar com base de dados da backet

--- script_infinitySolutions.sql
+++ script_infinitySolutions.sql
@@ -113,7 +113,7 @@ values
     
 insert into funcionario (codigo_funcionario, nome_funcionario, fkcodigo_cargo, cpf_funcionario, email_funcionario, senha_funcionario, status_funcionario, fkcodigo_instituicao)
 values
-	('vJ8Heo', "Luana", 1, "48825269803", "luacruz2014@gmail.com", "220206", "ativo", 100),
-    ('AB2Dce', "Caio", 2, "48825269803", "teste@gmail.com", "220206", "ativo", 100),
-    ('Gf2E14', "Patricia", 3, "48825269803", "teste2@gmail.com", "220206", "aguardando verificacao", 100);
+	('vJ8Heo', "Luana", 1, "48825269803", "luacruz2014@gmail.com", "220206", "ativo", 101),
+    ('AB2Dce', "Caio", 2, "48825269803", "teste@gmail.com", "220206", "ativo", 101),
+    ('Gf2E14', "Patricia", 3, "48825269803", "teste2@gmail.com", "220206", "aguardando verificacao", 101);
     
